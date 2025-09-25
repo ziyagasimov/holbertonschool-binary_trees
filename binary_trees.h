ceab9c5df14f6fa446eaf5c7b6b22926bc8eebd6
ziyagasimov <ziyagasimov0@gmail.com>
2025-09-25 20:53:05 +0000

    fcfgh

--- binary_trees.h
+++ binary_trees.h
@@ -5,12 +5,11 @@
 #include <stdlib.h>
 
 /**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
+ * struct binary_tree_s - İkili ağacın düyünü
+ * @n: Tam ədəd dəyəri
+ * @parent: Valideynə pointer
+ * @left: Sol uşağa pointer
+ * @right: Sağ uşağa pointer
  */
 struct binary_tree_s
 {
@@ -22,6 +21,7 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+/* Prototip */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
-#endif /* BINARY_TREES_H *
+#endif /* BINARY_TREES_H */
